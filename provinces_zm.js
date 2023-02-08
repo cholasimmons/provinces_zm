ffb368a1893413b44979519df029d9f0d3a5e076
Just.Chola <simmonsfrank@gmail.com>
2023-02-08 13:47:21 +0200

    Update provinces_zm.js

--- provinces_zm.js
+++ provinces_zm.js
@@ -1,19 +1,18 @@
 module.export = {
 	"Provinces": {
 		"Name": "Central",
-		{
-			"Capital": "Kabwe",
-			"Districts": {
-				"Name": "Chibombo",
-				"Capital": "Chibombo"
-			}
-		},
+		"Capital": "Kabwe",
+		"Districts": {
+			"Name": "Chibombo",
+			"Capital": "Chibombo"
+		}
+	},
+	{
 		"Name": "Copperbelt",
-		{
-			"Capital": "Ndola",
-			"Districts": {
-				"Name": "Chililabombwe"
-			}
+		"Capital": "Ndola",
+		"Districts": {
+			"Name": "Chililabombwe",
+			"Capital": "Chililabombwe"
 		}
 	}
 }
