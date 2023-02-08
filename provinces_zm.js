1aaf0eae38cbc298b3f6a36a2a5913d5c0b5e8b1
Just.Chola <simmonsfrank@gmail.com>
2023-02-08 13:43:12 +0200

    Create provinces_zm.js

---
+++ provinces_zm.js
@@ -0,0 +1,19 @@
+module.export = {
+	"Provinces": {
+		"Name": "Central",
+		{
+			"Capital": "Kabwe",
+			"Districts": {
+				"Name": "Chibombo",
+				"Capital": "Chibombo"
+			}
+		},
+		"Name": "Copperbelt",
+		{
+			"Capital": "Ndola",
+			"Districts": {
+				"Name": "Chililabombwe"
+			}
+		}
+	}
+}
